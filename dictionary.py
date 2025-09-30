f3d583118daded169a96b14e1bbab996acb57a45
xiyangab <xiyangh@nycstudents.net>
2025-09-30 06:24:18 -0700

    startind dic

---
+++ dictionary.py
@@ -0,0 +1,14 @@
+stock = [
+{
+    "name": "Apple"
+    "price": 1
+    "food_type": "Fruit"
+    "description": "Xiyang's amazing apple."
+},
+{
+    "name": "Orange"
+    "price": 1
+    "food_type": "Fruit"
+    "description": "Xiyang's amazing orange."
+}
+    ]
