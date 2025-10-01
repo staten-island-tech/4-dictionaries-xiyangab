efe43dbd7009a11fee1866b8f1a4633f892db016
xiyangab <xiyangh@nycstudents.net>
2025-10-01 06:33:20 -0700

    starting part 1

--- dictionary.py
+++ dictionary.py
@@ -1,14 +1,41 @@
 stock = [
 {
-    "name": "Apple"
-    "price": 1
-    "food_type": "Fruit"
+    "name": "Apple",
+    "price": 1,
+    "type": "Fruit",
     "description": "Xiyang's amazing apple."
 },
 {
-    "name": "Orange"
-    "price": 1
-    "food_type": "Fruit"
+    "name": "Orange",
+    "price": 1,
+    "type": "Fruit",
     "description": "Xiyang's amazing orange."
+},
+{
+    "name": "Coca-Cola",
+    "price": 1.5,
+    "type": "Drink",
+    "description": "Xiyang's amazing coca-cola."
+},
+{
+    "name": "Sprite",
+    "price": 1.5,
+    "type": "Drink",
+    "description": "Xiyang's amazing sprite."
+},
+{
+    "name": "Takis",
+    "price": 2.5,
+    "type": "Snack",
+    "description": "Xiyang's amazing takis."
+},
+{
+    "name": "Potato Chips",
+    "price": 2.5,
+    "type": "Snack",
+    "description": "Xiyang's amazing potato chips."
 }
-    ]
+]
+
+cart = input(f"Welcome to Xiyang's Amazing Store! What would you like the purchase?")
+
