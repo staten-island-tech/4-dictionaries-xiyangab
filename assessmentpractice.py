648632f7029795440eb94faa6c71314f724b92c9
xiyangab <xiyangh@nycstudents.net>
2025-10-07 06:26:12 -0700

    doing ap2

--- assessmentpractice.py
+++ assessmentpractice.py
@@ -1,37 +1,25 @@
+#assesment practice 1
 """ def lang(x):
-    print("English")
-lang("This is english language.") """
-
-""" input = input("type sentence in english or french")
-print(input.split())
-lf = []
-lf.append(input.split(" "))
-print(lf) """
-# if something > something:
-    #print english
-#elif something2 > something1:
-    #print french
-#elif something1 == something2:
-    #print french
-
-T = 0
-t = 0
-S = 0
-s = 0
-
-def lang(x):
     t = 0
+    s = 0
     for char in x:
-        if "T" or "t":
+        if char == "T" or char == "t":
             t = t + 1
-        elif "S" or "s":
+        elif char == "S" or char == "s":
             s = s + 1
+    if t > s:
+        print("english")
+    elif s >= t:
+        print("french")
 
-lang("s s t")
-
-if t > s:
-    print("english")
-elif s > t:
-    print("french")
-elif t == s:
-    print("french")
+lang("TtSsS")
+ """
+#assesment practice 2
+def spaces(y,t):
+    y = 0
+    t = 0
+    for char in y:
+        if char == "C":
+            y+=1
+    print(y)
+spaces("CCCCC","C.C.C.C..")
