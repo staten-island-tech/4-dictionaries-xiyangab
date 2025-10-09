123cd910dcf8c3e3bb55d71ae1dc85866ce5ac87
xiyangab <xiyangh@nycstudents.net>
2025-10-09 06:24:19 -0700

    doing dict

--- assessmentpractice.py
+++ assessmentpractice.py
@@ -15,10 +15,19 @@
 lang("TtSsS")
  """
 #assesment practice 2
-def spaces(y,t):
+""" def spaces(y,t):
     x = 0
     for char in y and t:
         if char == "C":
             x = x + 1
     print(x)
-spaces("CCCCCCCCCC","C.C.C.C.C.")
+spaces("CCCCCCCCCC","C.C.C.C.C.") """
+
+#correct answer:
+def occupied(n,y,t):
+    both = 0
+    for i in range(n):
+        if (y[i] == "C" and t[i] == "C"):
+            both +=1
+    return both
+print(occupied(5, "CCC..", "C.C.C"))
--- dictionary.py
+++ dictionary.py
@@ -36,17 +36,21 @@ stock = [
     "description": "Xiyang's amazing potato chips."
 }
 ]
-
 print("Welcome to Xiyang's amazing store!")
-
 names = [item["name"] for item in stock]
 print("Stock:", names)
  #i should make a list instead of buy 
+cart = []
 buy = input("What would you like to buy?")
+cart.append(buy)
 cont = input("Do you want to continue? Type 'Yes' or 'No'")
-
 while cont != "No":
-    print(f"Your cart: {cont}")
+    print(f"Your cart: {cart}")
+    buy = input("What would you like to buy?")
     cont = input("Do you want to continue? Type 'Yes' or 'No'")
     if cont == 'Yes':
         buy = input("What would you like to buy?")
+        cart.append(buy)
+        print(f"Your cart: {cart}")
+if cont == "No":
+    print(f"Your cart: {cart}")
