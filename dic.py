21d68045e93a958f125b2545110066f777feb658
xiyangab <xiyangh@nycstudents.net>
2025-10-16 05:44:08 -0700

    Create dic.py

---
+++ dic.py
@@ -0,0 +1,43 @@
+stock = [
+{
+    "name": "Apple",
+    "price": 1,
+    "type": "Fruit",
+    "description": "Xiyang's amazing apple."
+},
+{
+    "name": "Orange",
+    "price": 1,
+    "type": "Fruit",
+    "description": "Xiyang's amazing orange."
+},
+{
+    "name": "Coca-Cola",
+    "price": 1.5,
+    "type": "Drink",
+    "description": "Xiyang's amazing coca-cola."
+},
+{
+    "name": "Sprite",
+    "price": 1.5,
+    "type": "Drink",
+    "description": "Xiyang's amazing sprite."
+},
+{
+    "name": "Takis",
+    "price": 2.5,
+    "type": "Snack",
+    "description": "Xiyang's amazing takis."
+},
+{
+    "name": "Potato Chips",
+    "price": 2.5,
+    "type": "Snack",
+    "description": "Xiyang's amazing potato chips."
+}
+]
+print("welcome to xiyangs amazing store!")
+print("stock:")
+for index, item in enumerate(stock):
+    print(index, ":", item["name"])
+print("what would you like to buy?")
